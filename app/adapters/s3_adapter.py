9fc857a83983a692c2337818f4963aae2ea175ad
sSp3ctr <alexandr.shvets.ua@gmail.com>
2025-03-12 11:24:07 +0200

    asinc boto update

--- app/adapters/s3_adapter.py
+++ app/adapters/s3_adapter.py
@@ -1,4 +1,4 @@
-import boto3
+import aioboto3
 from botocore.exceptions import NoCredentialsError, BotoCoreError
 from app.adapters.base import BaseStorageAdapter
 from typing import Dict, Optional
@@ -9,31 +9,29 @@ class S3Adapter(BaseStorageAdapter):
         super().__init__(config)
         self.bucket_name = config["bucket_name"]
         self.region = config["region"]
-        self.s3_client = boto3.client(
-            "s3",
-            aws_access_key_id=config["access_key"],
-            aws_secret_access_key=config["secret_key"],
-            region_name=self.region,
-        )
+        self.access_key = config["access_key"]
+        self.secret_key = config["secret_key"]
 
     async def upload_file(self, file_path: str, file_name: str, folder: Optional[str] = None) -> Dict[str, str]:
         """
-        Загружает файл в S3, поддерживает каталоги (префиксы).
+        Загружает файл в S3 асинхронно, поддерживает каталоги (префиксы).
         :param file_path: путь к файлу
         :param file_name: имя файла
         :param folder: подкаталог в S3 (если нужен)
         :return: словарь с file_id и url
         """
         try:
-            # Если указан подкаталог, добавляем его к пути
             s3_key = f"{folder}/{file_name}" if folder else file_name
 
-            # Загружаем файл в S3
-            self.s3_client.upload_file(file_path, self.bucket_name, s3_key)
+            async with aioboto3.Session().client(
+                "s3",
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_key,
+                region_name=self.region,
+            ) as s3_client:
+                await s3_client.upload_file(file_path, self.bucket_name, s3_key)
 
-            # Формируем URL файла
             file_url = f"https://{self.bucket_name}.s3.{self.region}.amazonaws.com/{s3_key}"
-
             return {"file_id": s3_key, "url": file_url}
 
         except NoCredentialsError:
